78ee43bccb389016f1240a6dbb321627f6d6f8e6
Karthik-Kalburgi <kalburgikarthik0105@gmail.com>
2024-11-15 14:19:55 +0530

    updated counter values

--- 02CounterProject/src/App.jsx
+++ 02CounterProject/src/App.jsx
@@ -1,33 +1,38 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
-import "./App.css";
-
-function App() {
-  let [counter, setcounter] = useState(16)
-
-  const addValue = ()=>{
-      console.log("Clicked" , counter);
-      counter = counter + 1;
-    let addans =   setcounter(counter)
-    addans
-      
-  }
-  const removeValue =()=>{
-  setcounter(counter - 1);
-    
-  }
+const App = () => {
+  let [counter, setcounter] = useState(15);
+  const AddValue = () => {
+    counter = counter + 1;
+    setcounter(counter);
+  };
+  const removeValue = () => {
+    counter = counter - 1;
+    setcounter(counter);
+  };
   return (
     <>
-      <h1>Chai aur react</h1>
-      <h2> Counter : {counter} </h2>
-      <button onClick={addValue} >Add Value</button>
-      <br></br>
-      <button  onClick={removeValue} >Remove Value {counter} </button>
-      <footer>
-        <p>{counter}</p>
-      </footer>
+      <h1 className="text-center mt-8 text-green-600 font-bold text-2xl pb-4 ">
+        Chai aur React{" "}
+      </h1>
+      <h2 className="text-center text-red-800 font-semibold text-xl pb-16 ">
+        Counter Project{" "}
+      </h2>
+      <div className="flex justify-center items-center pb-5">
+        <button onClick={AddValue} className="bg-green-500 rounded-lg text-lg">
+          AddValue - {counter}{" "}
+        </button>
+      </div>
+      <div className="flex items-center justify-center">
+        <button
+          onClick={removeValue}
+          className="bg-green-500 rounded-lg text-lg"
+        >
+          RemoveValue {counter}{" "}
+        </button>
+      </div>
     </>
   );
-}
+};
 
 export default App;
